86fce2a19756cb646ef6be2d736260473e0b3e6c
Dilnaz <dilnaz.m13@gmail.com>
2025-05-16 02:51:13 +0500

    oaooa

--- app/part/[partId]/page.tsx
+++ app/part/[partId]/page.tsx
@@ -2,8 +2,16 @@ export const dynamic = 'force-dynamic';
 import { PartState } from "@/lib/helper/part_state";
 import PartPage from "./partPage";
 import prisma from "@/lib/prisma";
+import { notFound } from 'next/navigation';
 
-export default async function Part({params}: {params: {partId: string}}) {
-  const partInfo = await prisma.part.findUnique({where: {id: parseInt(params.partId)}}) as PartState;
-  return <PartPage part={partInfo}/>;
+export default async function Part({ params }: { params: { partId: string } }) {
+  const partInfo = await prisma.part.findUnique({
+    where: { id: parseInt(params.partId) }
+  }) as PartState | null;
+
+  if (!partInfo) {
+    return notFound(); 
+  }
+
+  return <PartPage part={partInfo} />;
 }
